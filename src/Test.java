b50db6b1419db3fe162794483dd569e77ab309c3
tbpwang <tbpwang@gmail.com>
2016-05-19 15:03:39 +0800

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Joe Wang, tbpwang@gmail.com
+ * 2016/5/19.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello,guy!");
+    }
+}
